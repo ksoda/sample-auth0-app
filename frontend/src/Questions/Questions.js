a2708b509b6d1313f36d65190f9569f93a17a089
Ken SONODA <ksoda@users.noreply.github.com>
2019-07-22 00:57:00 +0900

    Add Question

--- frontend/src/Questions/Questions.js
+++ frontend/src/Questions/Questions.js
@@ -26,7 +26,7 @@ class Questions extends Component {
           {this.state.questions &&
             this.state.questions.map(question => (
               <div key={question.id} className="col-sm-12 col-md-4 col-lg-3">
-                <Link to={`/question/${question.id}`}>
+                <Link to={`/questions/${question.id}`}>
                   <div className="card text-white bg-success mb-3">
                     <div className="card-header">
                       Answers: {question.answers}
