bea302360163d88b7c59835307378773f3e7b419
Ken SONODA <ksoda@users.noreply.github.com>
2019-07-27 17:47:50 +0900

    Guard checking

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,14 +9,24 @@ import SecuredRoute from "./SecuredRoute/SecuredRoute";
 import auth0Client from "./Auth";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      checkingSession: true
+    };
+  }
   async componentDidMount() {
-    if (this.props.location.pathname === "/callback") return;
+    if (this.props.location.pathname === "/callback") {
+      this.setState({ checkingSession: false });
+      return;
+    }
     try {
       await auth0Client.silentAuth();
       this.forceUpdate();
     } catch (err) {
       if (err.error !== "login_required") console.log(err.error);
     }
+    this.setState({ checkingSession: false });
   }
   render() {
     return (
@@ -25,7 +35,11 @@ class App extends Component {
         <Route exact path="/" component={Questions} />
         <Route exact path="/questions/:questionId" component={Question} />
         <Route exact path="/callback" component={Callback} />
-        <SecuredRoute path="/new-question" component={NewQuestion} />
+        <SecuredRoute
+          path="/new-question"
+          component={NewQuestion}
+          checkingSession={this.state.checkingSession}
+        />
       </div>
     );
   }
